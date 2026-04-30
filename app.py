4637881af4760bea8a6e7c03ec64038d16302e8e
AhmedAAYounes <zyad492765@gmail.com>
2026-04-30 09:31:41 +0300

    Adjust hero title font size and weight

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ st.markdown("""
     .hero-title {
         color: #89CFF0 !important;
         text-align: center;
-        font-size: 70px !important; /* حجم ضخم جداً */
-        font-weight: 900 !important;
+        font-size: 60px !important; /* حجم ضخم جداً */
+        font-weight: 700 !important;
         margin-bottom: 0px !important;
         padding-bottom: 0px !important;
         text-shadow: 3px 3px 6px rgba(137, 207, 240, 0.3);
